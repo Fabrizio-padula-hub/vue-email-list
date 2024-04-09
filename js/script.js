abee7a9dc4e52821e0e76e80347b30460697a0b2
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-04-09 14:54:36 +0200

    feat: preparation of html and js structure

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+
+const { createApp } = Vue;
+
+
+
+createApp({
+  data() {
+    return {
+      
+      
+    };
+  },
+  methods: {
+    
+    },
+  
+    
+}).mount('#app');
